adf959b2bb319592d06dee7e4551318d607e0128
danny-thet <thetnaingoosru@gmail.com>
2022-08-17 13:02:01 +0200

    chore: styling boxes

--- src/components/PokemonMain.tsx
+++ src/components/PokemonMain.tsx
@@ -1,24 +1,31 @@
-import { Box, Flex, Heading, Input } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Input, Spacer } from "@chakra-ui/react";
 export const PokemonMain = () => {
 	return (
 		<Box textAlign="center" h="100%" minH="100vh" backgroundColor="#FEF9E7">
-			<Heading pt="50px" as="h1">
+			<Heading
+				py="50px"
+				as="h1"
+				background="white"
+				boxShadow="0px 0px 5px 5px lightgray"
+				fontSize="400%"
+			>
 				Who is That Pokemon?
 			</Heading>
-			<Flex flexWrap="wrap" mx="auto" width="80%" h="100%" minH="100vh">
+			<Flex flexWrap="wrap" mx="auto" width="80%" h="100%" minH="80vh">
 				<Box
 					mx="auto"
 					marginTop="50px"
 					width="500px"
 					minH="300px"
-					maxH="500px"
+					maxH="700px"
 					border="2px"
-					borderColor="gray.200"
+					borderColor="white"
 					borderRadius="4px"
 					bgImg="url(/background.png)"
 					bgPos="center"
 					bgRepeat="no-repeat"
 					bgSize="cover"
+					boxShadow="5px 5px white"
 				/>
 				<Box
 					w="500px"
@@ -26,17 +33,35 @@ export const PokemonMain = () => {
 					mx="auto"
 					my="50px"
 					border="2px"
-					borderColor="gray.200"
+					borderColor="white"
 					borderRadius="4px"
+					backgroundColor="white"
+					boxShadow=" 5px 5px tomato"
 				>
-					<Box>Title</Box>
+					<Heading py="50px" fontSize="300%">
+						Who is That Pokemon?
+					</Heading>
 					<Box>
-						<Input border="2px" borderColor="gray" mx="5px" w="300px"></Input>
+						<Input
+							border="2px"
+							textAlign="center"
+							borderColor="lightgray"
+							color="tomato"
+							focusBorderColor="tomato"
+							placeholder="GUESS THE NAME"
+							_placeholder={{ opacity: 0.4, color: "inherit" }}
+							mx="5px"
+							w="70%"
+						></Input>
 					</Box>
-					<Box mx="auto" display="inline-block">
-						<Flex>
-							<Box>Scores</Box>
-							<Box>Hints</Box>
+					<Box mx="auto" display="inline-block" my="30px" w="70%">
+						<Flex justifyContent="space-between">
+							<Heading as="h1">Scores: 0</Heading>
+							<Box>
+								<Button colorScheme="red" size="lg">
+									Next
+								</Button>
+							</Box>
 						</Flex>
 					</Box>
 				</Box>
