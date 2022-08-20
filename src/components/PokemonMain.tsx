6da6785f97bc5b7ac575caf1b4cdb3b56d0dfa1c
danny-thet <thetnaingoosru@gmail.com>
2022-08-20 21:17:03 +0700

    feat: fetch pokemon

--- src/components/PokemonMain.tsx
+++ src/components/PokemonMain.tsx
@@ -1,5 +1,59 @@
-import { Box, Button, Flex, Heading, Input, Spacer } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Input, Image } from "@chakra-ui/react";
+import { Pokemon, PokemonClient } from "pokenode-ts";
+import React, { useEffect, useState } from "react";
+
+const offset = 0;
+const limit = 650;
+
 export const PokemonMain = () => {
+	const [pokemonData, setPokemonData] = useState<Pokemon>();
+	const [isCorrect, setIsCorrect] = useState<boolean>(false);
+	const pokemonName = pokemonData?.name;
+
+	const characterCount = pokemonName?.length ?? 0;
+	const toRemove = characterCount - 2;
+
+	const toProduce = toRemove > 0 ? new Array(toRemove + 1).join(" _") : "";
+
+	const hiddenName = pokemonName?.slice(0, -toRemove) + toProduce;
+
+	const [scores, setScores] = useState<number>(0);
+
+	const fetchPokemon = async (pokemonId: number) => {
+		const api = new PokemonClient();
+		await api
+			.getPokemonById(pokemonId)
+			.then((data) => setPokemonData(data))
+			.catch((error) => console.error(error));
+	};
+
+	const generateRandomNumber = () => {
+		const id = Math.floor(Math.random() * (limit - offset + 1) + offset);
+
+		return id;
+	};
+
+	const randomId = generateRandomNumber();
+
+	useEffect(() => {
+		fetchPokemon(randomId);
+	}, []);
+
+	const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+		const answer = event.currentTarget.value;
+		if (event.key === "Enter") {
+			if (answer === pokemonData?.name) {
+				setScores(scores + 1);
+			}
+			setIsCorrect(true);
+			setTimeout(async () => {
+				const newId = generateRandomNumber();
+				await fetchPokemon(newId);
+				setIsCorrect(false);
+			}, 2000);
+		}
+	};
+
 	return (
 		<Box textAlign="center" h="100%" minH="100vh" backgroundColor="#FEF9E7">
 			<Heading
@@ -17,16 +71,22 @@ export const PokemonMain = () => {
 					marginTop="50px"
 					width="500px"
 					minH="300px"
-					maxH="700px"
-					border="2px"
-					borderColor="white"
+					maxH="500px"
 					borderRadius="4px"
 					bgImg="url(/background.png)"
 					bgPos="center"
 					bgRepeat="no-repeat"
 					bgSize="cover"
-					boxShadow="5px 5px white"
-				/>
+					boxShadow="0px 0px 5px 5px lightgray"
+				>
+					{pokemonData && (
+						<Image
+							src={pokemonData?.sprites?.front_shiny ?? ""}
+							filter="brightness(0%)"
+							alt="Pokemon"
+						/>
+					)}
+				</Box>
 				<Box
 					w="500px"
 					maxH="500px"
@@ -39,7 +99,7 @@ export const PokemonMain = () => {
 					boxShadow=" 5px 5px tomato"
 				>
 					<Heading py="50px" fontSize="300%">
-						Who is That Pokemon?
+						{isCorrect ? pokemonName : hiddenName}
 					</Heading>
 					<Box>
 						<Input
@@ -52,11 +112,12 @@ export const PokemonMain = () => {
 							_placeholder={{ opacity: 0.4, color: "inherit" }}
 							mx="5px"
 							w="70%"
-						></Input>
+							onKeyDown={(event) => handleEnter(event)}
+						/>
 					</Box>
 					<Box mx="auto" display="inline-block" my="30px" w="70%">
 						<Flex justifyContent="space-between">
-							<Heading as="h1">Scores: 0</Heading>
+							<Heading as="h1">Scores: {scores}</Heading>
 							<Box>
 								<Button colorScheme="red" size="lg">
 									Next
